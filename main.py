2fbfded5a962d5cf8669fd117bd956e7e372e1bc
Hadiannn1 <127890637+Hadiannn1@users.noreply.github.com>
2023-03-22 16:34:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import openai
 from aiogram import Bot, Dispatcher, types, executor
 
-tk = '6279616551:AAGQxe5VII1x1bRGkOlJ7-3BOrV5i3QV7Ss'
-openai.api_key= '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'
+token = '<ISI DENGAN TOKEN BOTFATHER>'
+openai.api_key= '<ISI DENGAN API DARI OPEN.AI>'
 
-bot = Bot(token=tk)
+bot = Bot(token=token)
 dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
 async def user_come(message: types.Message):
-	await message.answer('Silahkan tanyakan apa saja')
-
-@dp.message_handler(commands=['help'])
-async def user_come(message: types.Message):
-	await message.answer('Jika butuh bantuan hubungi @nyumiXDii')
-
-@dp.message_handler(commands=['donate'])
-async def user_come(message: types.Message):
-    await message.answer('silahkan donasi ke https://sht.moe/oeABa')
+	await message.answer('Hai, Silahkan tanyakan apa saja')
 
 @dp.message_handler(lambda message: message.text)
 async def ai_answer(message: types.Message):
@@ -25,5 +17,5 @@ async def ai_answer(message: types.Message):
 	parse = respon['choices'][0]['text']
 	await message.reply(parse)
 	
-print('Bot Running @nyumichatbot')	
+print('Bot Running')	
 executor.start_polling(dp)
